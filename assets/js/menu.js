62b4fbab230c8ce2df40ef945d81be9f9b898617
Krithik Rao <krdevmail@gmail.com>
2017-08-09 18:26:17 -0500

    Updated Toggle Navbar
    Toggle Navbar now rendered by handlebars completely.
    ID's replaced by Classes where applicable. CSS simplified on numerous
    accounts.
    Jquery now used to update menu bar shown. Menu bar is now shifted by
    position rather than being set to width 0.

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,28 +1,20 @@
 function showMenu () {
-  var x = document.getElementById("toggle-menu");
-  var y = document.getElementById("overlay");
-  x.style.width = '330px';
-  y.style.display = 'block';
+  $(".toggle-menu").addClass('active');
+  $("#overlay").show();
 }
 
 function hideMenu () {
-  var x = document.getElementById("toggle-menu");
-  var y = document.getElementById("overlay");
-  x.style.width = '0px';
-  y.style.display = 'none';
+  $(".toggle-menu").removeClass('active');
+  $("#overlay").hide();
 }
   
 $(document).ready(function(){
-  /*$('.menu a').on('click', function({
-    $('.menu a').removeClass('active');
-    $(this).addClass('active');
-  });*/
-  $("#toggle-information-tab").click(function(){
-    $("#toggle-sub-tab-one").toggle();
-  });
-  
-  $("#toggle-religious-tab").click(function(){
-    $("#toggle-sub-tab-two").toggle();
+  $('.toggle-tab').on('click', function(){
+    var isActive = $(this).hasClass('active');
+    $('.toggle-tab').removeClass('active');
+    if(!isActive){
+      $(this).addClass('active');
+    }
   });
 });
 
